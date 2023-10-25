50a7fc19a2901cd9860d9d79510beae72866eb26
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2023-10-25 10:47:08 +0200

    feed.js datas

--- Feed/feed.js
+++ Feed/feed.js
@@ -1,2 +1,4 @@
 // change the like icon when hovering over or clicked on the like
 // minden poszt kártyát itt hozunk létre, nem pedig html-ben
+const dataUrl = 'db.json';
+var id, profilPickUrl, username, text, postTime;
