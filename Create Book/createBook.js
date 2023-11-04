9cb825b5a15f049e0257376d9191a9fdfababef5
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2023-11-04 14:53:57 +0100

    update createBook - 11.04
    drag and drop funkció a képfeltöltéshez

--- Create Book/createBook.js
+++ Create Book/createBook.js
@@ -1,4 +1,25 @@
-const TargetChoose = document.getElementById('targetAudienceChoose');
-TargetChoose.addEventListener('click', e=>{
-    TargetChoose.classList.add('dropdown-down');
+//* DRAG AND DROP IMAGE TO COVER PHOTO 
+
+const dropArea = document.getElementById('dropArea');
+const inputPicture = document.getElementById('inputPicture');
+const imgView = document.getElementById('imgView');
+
+inputPicture.addEventListener("change", uploadImage);
+
+function uploadImage(){
+    let imgLink = URL.createObjectURL(inputPicture.files[0]);
+    imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+}
+
+dropArea.addEventListener('dragover', (e)=>{
+    e.preventDefault();
+})
+
+dropArea.addEventListener('drop', (e)=>{
+    e.preventDefault();
+    inputPicture.files = e.dataTransfer.files;
+    uploadImage();
+    console.log(inputPicture.value);
 })
