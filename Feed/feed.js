5d5852478a417df0d784f0fe10cf3785830b72a0
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2023-11-29 17:01:35 +0100

    @mayer-hedda  Help me

--- Feed/feed.js
+++ Feed/feed.js
@@ -47,13 +47,13 @@ fetch(dataUrl)
                             <div class="d-flex flex-column align-items-center emptyLike">
                                 
 
-                            <div class="like" id="like">
+                            <div class="like visible" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
                                 </svg>
                             </div>
 
-                            <div class="liked" id="like" hidden>
+                            <div class="liked hidden" style="margin-right: 10px;">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                                 </svg>
@@ -75,23 +75,27 @@ fetch(dataUrl)
     }
     )
 
-const like = document.querySelector('#like.like');
-const liked = document.getElementsByClassName('#like.liked');
+const like = document.querySelector('.like');
+const liked = document.querySelector('.liked');
 
 
 like.addEventListener('click', function(){
     e.preventDefault();
     console.log("lájkoltad");
-    like.hidden = true;
+    // Látható elem elrejtése
 
-    liked.hidden = false;
+  like.classList.add('hidden');
+  like.classList.remove('visible')
+  // Rejtett elem megjelenítése
+  liked.classList.remove('hidden');
+  liked.classList.add('visible');
 })
 
 liked.addEventListener('click', function(){
     e.preventDefault();
     console.log("ki lájkoltad");
-    liked.hidden = true;
-
-    like.hidden = false;
+    hiddenLike.classList.add('hidden');
+  // Látható elem megjelenítése
+  visibleLike.classList.remove('hidden');
 })
 
