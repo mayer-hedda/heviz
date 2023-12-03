79b18f79004d046f2801cac77ab6842cbafc3ef9
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2023-12-03 15:29:18 +0100

    update genHome - 12/03
    - update genHome js
    - kiegészítettem a createBook-ot az adult contenttel
    - a json-be kicseréltem 2 blokk teszt adatot

--- General-HomePage/GenHome.js
+++ General-HomePage/GenHome.js
@@ -2,7 +2,15 @@
 const dataURL = './db.json';
 const s1_bigCard_div = document.getElementById('bigCard-Pic');
 const s1_bigCard_h2 = document.getElementById('s1-bigCard-h2');
+const s1_bigCard_user = document.getElementById('s1-bigCard-user');
 const s1_bigCard_p = document.getElementById('s1-bigCard-p');
+
+const s2_mediumC_picDiv = document.getElementById('s2-mediumC-pic');
+const s2_mediumC_h2 = document.getElementById('s2-mediumC-h2');
+const s2_mediumC_user = document.getElementById('s2-mediumC-user');
+const s2_mediumC_p = document.getElementById('s2-mediumC-p');
+
+const s2_first_row = document.getElementById('s2-first-row');
 
 fetch(dataURL)
     .then(response => {
@@ -14,14 +22,54 @@ fetch(dataURL)
 
     .then(data => {
         const bigCard = data.bigCard;
-        for(const bigCard of data.bigCard){
+        for (const bigCard of data.bigCard) {
             // console.log(`username: ${bigCard.username}`);
             s1_bigCard_div.innerHTML = `
             
-                <img src="${bigCard.imgURL}">
+                <img src="${bigCard.imgURL}" alt="${bigCard.title} cover">
             
-            `
+            `;
+
+            s1_bigCard_h2.innerText = `${bigCard.title}`;
+            s1_bigCard_p.innerText = `${bigCard.description}`;
+            s1_bigCard_user.innerText = `@${bigCard.username}`;
+
+
         }
+
+        const mediumCard_first = data.mediumCard[0];
+        s2_mediumC_picDiv.innerHTML = `
+
+            <img class="medium-pic" src="${mediumCard_first.imgURL}" alt="${mediumCard_first.title} cover">
+        
+        `;
+        s2_mediumC_h2.innerText = `${mediumCard_first.title}`;
+        s2_mediumC_user.innerText = `@${mediumCard_first.username}`;
+        s2_mediumC_p.innerText = `${mediumCard_first.description}`;
+
+        // ! HA BENT VAN A FOR AKKOR VALAMIÉRT ÖSSZESZARJA MAGÁT A BÖNGÉSZŐ
+        // for (let i = 1; i = 4; i++) {
+        //     const mediumCard = data.mediumCard[i];
+        //     console.log(mediumCard);
+            
+        //     s2_first_row.innerHTML = `
+            
+        //             <div class="col-3">
+        //                 <div class="cover-photo">
+
+        //                     <img src="${mediumCard.imgURL}" alt="${mediumCard.title}" class="cover">
+        //                     <div class="overlay">
+        //                         <p class="book-title">${mediumCard.title}</p>
+        //                         <p class="author-p">${mediumCard.username}</p>
+        //                         <a role="button" class="cover-btn" href="#">Start Reading</a>
+        //                     </div>
+        //                 </div>
+        //             </div>
+            
+        //     `;
+        // }
+
+
     })
 
 // ------------------ hover effekt a borítóknak -----------------------
