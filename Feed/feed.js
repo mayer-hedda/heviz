5166535407deaa90fe7b05477d8ce659511f6073
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2023-12-06 08:58:40 +0100

    update 12/05

--- Feed/feed.js
+++ Feed/feed.js
@@ -57,13 +57,13 @@ fetch(dataUrl)
                                 </svg>
                             </div>
                                 
-                                <span class="text-center like">Like</span>
+                                
                             </div>
                             <div class="d-flex flex-column align-items-center">
                                 <div class="share"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
                                 <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
                               </svg></div>
-                                <span class="text-center share">Share</span>
+                               
                             </div>
                         </div>
                     </div>
