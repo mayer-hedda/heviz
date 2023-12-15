d09a7b1af535a5d9e9d586331ab27ba2bdb89f6b
Mayer Hedda <mayer.hedda@gmail.com>
2023-12-15 23:14:50 +0100

    postLike

--- Api/index.js
+++ Api/index.js
@@ -737,3 +737,51 @@
         })
         .catch(error => console.log('error', error));
     }
+
+
+    /**
+     * @param {JSON} raw = {
+     *      "postId": 1
+     *  }
+     * 
+     * @return
+        * 200: Successfully liked the post
+        * 
+        * 401:
+            * User hasn't token
+            * Invalid token
+            * The token has expired
+        * 
+        * 422:
+            * error: if this post doesn't exist
+                * postlikeError   
+     */
+    function postLike(raw) {
+        var myHeaders = new Headers();
+
+        myHeaders.append("Content-Type", "application/json");
+        var storedToken = localStorage.getItem("Token");
+        if(storedToken) {
+            myHeaders.append("Token", storedToken);
+        }
+
+        var postData = JSON.stringify(raw);
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: postData,
+        redirect: 'follow'
+        };
+
+        fetch("http://127.0.0.1:8080/CyberRead-1.0-SNAPSHOT/webresources/postlike/postLike", requestOptions)
+        .then(response => {
+            return response.json().then(data => {
+                return { 
+                    status: response.status, 
+                    data: data 
+                };
+            });
+        })
+        .catch(error => console.log('error', error));
+    }
