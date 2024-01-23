421571ad2fb4d2408f67663c78463a15f0e3cf7a
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2024-01-23 17:27:56 +0100

    Update Publisher profile - 01/23
    - profilkép feltöltés popup + javascript --> még nem küld a backendnek adatot

--- Publisher-Profile/p-profile.js
+++ Publisher-Profile/p-profile.js
@@ -1,9 +1,30 @@
 const our_books = document.getElementById('our-books');
 const our_posts = document.getElementById('our-posts');
 
-let posts = false;
-function SwitchToPosts() {
-    posts = true;
 
-    // switch book to invisible
+// Upload profile picture
+const drop_area_picture = document.getElementById('drop-area-picture');
+const inputPicture = document.getElementById('inputPicture');
+const imgView = document.getElementById('imgView');
+
+inputPicture.addEventListener('change', uploadImage);
+
+function uploadImage() {
+    let imgLink = URL.createObjectURL(inputPicture.files[0]);
+    imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+
+    var img = new Image();
+    img.src = imgLink;
+
+    img.onload = function(){
+        height = img.naturalHeight;
+        width = img.naturalWidth;
+        console.log("A kép magassága: " + height);
+        console.log("A kép szélessége: " + width);
+
+        // ! ide kell egy arány vizsgálat, és meghatározni milyen arányú képeket tölthet fel
+    }
+    
 }
