4c8c6295d8990d3d3425794164ce5bcbeee204c7
Mayer Hedda <mayer.hedda@gmail.com>
2024-02-24 23:53:34 +0100

    pdf viewer

--- PDF/script.js
+++ PDF/script.js
@@ -1,91 +1,69 @@
-const url = './pdf.pdf';
+var myState = {
+    pdf: null,
+    currentPage: 1,
+    zoom: 1
+}
 
-let pdfDoc = null,
-  pageNum = 1,
-  pageIsRendering = false,
-  pageNumIsPending = null;
+pdfjsLib.getDocument('./pdf.pdf').then((pdf) => {
 
-const scale = 1.5,
-  canvas = document.querySelector('#pdf-render'),
-  ctx = canvas.getContext('2d');
+    myState.pdf = pdf;
+    render();
+});
+function render() {
+    myState.pdf.getPage(myState.currentPage).then((page) => {
+ 
+        var canvas = document.getElementById("pdf_renderer");
+        var ctx = canvas.getContext('2d');
 
-// Render the page
-const renderPage = num => {
-  pageIsRendering = true;
-
-  // Get page
-  pdfDoc.getPage(num).then(page => {
-    // Set scale
-    const viewport = page.getViewport({ scale });
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    const renderCtx = {
-      canvasContext: ctx,
-      viewport
-    };
-
-    page.render(renderCtx).promise.then(() => {
-      pageIsRendering = false;
-
-      if (pageNumIsPending !== null) {
-        renderPage(pageNumIsPending);
-        pageNumIsPending = null;
-      }
+        var viewport = page.getViewport(myState.zoom);
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+ 
+        page.render({
+            canvasContext: ctx,
+            viewport: viewport
+        });
     });
+}
+document.getElementById('go_previous').addEventListener('click', (e) => {
+    if (myState.pdf == null || myState.currentPage == 1)
+        return;
+    myState.currentPage -= 1;
+    document.getElementById("current_page").value = myState.currentPage;
+    render();
+});
+document.getElementById('go_next').addEventListener('click', (e) => {
+    if (myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages)
+        return;
+    myState.currentPage += 1;
+    document.getElementById("current_page").value = myState.currentPage;
+    render();
+});
+document.getElementById('current_page').addEventListener('keypress', (e) => {
+    if (myState.pdf == null) return;
 
-    // Output current page
-    document.querySelector('#page-num').textContent = num;
-  });
-};
+    // Get key code 
+    var code = (e.keyCode ? e.keyCode : e.which);
 
-// Check for pages rendering
-const queueRenderPage = num => {
-  if (pageIsRendering) {
-    pageNumIsPending = num;
-  } else {
-    renderPage(num);
-  }
-};
+    // If key code matches that of the Enter key 
+    if (code == 13) {
+        var desiredPage =
+            document.getElementById('current_page').valueAsNumber;
 
-// Show Prev Page
-const showPrevPage = () => {
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  queueRenderPage(pageNum);
-};
-
-// Show Next Page
-const showNextPage = () => {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
-};
-
-// Get Document
-pdfjsLib
-  .getDocument(url)
-  .promise.then(pdfDoc_ => {
-    pdfDoc = pdfDoc_;
-
-    document.querySelector('#page-count').textContent = pdfDoc.numPages;
-
-    renderPage(pageNum);
-  })
-  .catch(err => {
-    // Display error
-    const div = document.createElement('div');
-    div.className = 'error';
-    div.appendChild(document.createTextNode(err.message));
-    document.querySelector('body').insertBefore(div, canvas);
-    // Remove top bar
-    document.querySelector('.top-bar').style.display = 'none';
-  });
-
-// Button Events
-document.querySelector('#prev-page').addEventListener('click', showPrevPage);
-document.querySelector('#next-page').addEventListener('click', showNextPage);
+        if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+            myState.currentPage = desiredPage;
+            document.getElementById("current_page").value = desiredPage;
+            render();
+        }
+    }
+});
+document.getElementById('zoom_in').addEventListener('click', (e) => {
+    if (myState.pdf == null) return;
+    myState.zoom += 0.5;
+    render();
+});
+document.getElementById('zoom_out').addEventListener('click', (e) => {
+    if (myState.pdf == null) return;
+    myState.zoom -= 0.5;
+    render();
+});
