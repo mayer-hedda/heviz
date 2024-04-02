717ec694b2b7596982998b2974f47fbb763ea9c2
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2024-04-02 16:49:36 +0200

    Feed respo elkezdése - 04/02

--- Feed/feed.js
+++ Feed/feed.js
@@ -196,13 +196,7 @@ async function LoadPosts(response) {
                                     </svg></button>
                                     
                                 </div>
-                                    <div class="d-flex flex-column align-items-center">
-                                        <button class="border-0 bg-transparent share"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" class="bi bi-share-fill" viewBox="0 0 16 16">
-                                            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
-                                            </svg>
-                                        </button>
-                                        
-                                    </div>
+                                    
                                 </div>
                             </div>
                     </div>
