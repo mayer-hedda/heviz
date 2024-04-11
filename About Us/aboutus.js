b72e902cff9c581bfe7663f6eb1a6b6685e32b3e
Vivyan715 <kiss.vivien.brigitta@gmail.com>
2024-04-11 13:57:20 +0200

    About us respo

--- About Us/aboutus.js
+++ About Us/aboutus.js
@@ -1,24 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const kvb_img = document.getElementById('kvb-img');
+    const kvb_name = document.getElementById('KissVivien');
     const kvb_section = document.getElementById('KissVivienBrigitta');
 
     const mha_img = document.getElementById('mha-img');
+    const mha_name = document.getElementById('MayerHedda');
     const mha_section = document.getElementById('MayerHeddaAdrienn');
 
     const szv_img = document.getElementById('szv-img');
+    const szv_name = document.getElementById('SzirenyiVivien');
     const szv_section = document.getElementById('SzirenyiVivi');
 
-    kvb_img.addEventListener('click', function(){
-        kvb_section.scrollIntoView({behavior: 'smooth'});
+    kvb_img.addEventListener('click', function () {
+        kvb_section.style.scrollMarginTop = '80px';
+        kvb_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
 
-    mha_img.addEventListener('click', function(){
-        mha_section.scrollIntoView({behavior: 'smooth'});
+    kvb_name.addEventListener('click', function(){
+        kvb_section.style.scrollMarginTop = '80px';
+        kvb_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    })
+
+    mha_img.addEventListener('click', function () {
+        mha_section.style.scrollMarginTop = '40px';
+        mha_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
 
-    szv_img.addEventListener('click', function(){
-        szv_section.scrollIntoView({behavior: 'smooth'});
+    mha_name.addEventListener('click', function(){
+        mha_section.style.scrollMarginTop = '40px';
+        mha_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    })
+
+    szv_img.addEventListener('click', function () {
+        szv_section.style.scrollMarginTop = '40px';
+        szv_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
+
+    szv_name.addEventListener('click', function(){
+        szv_section.style.scrollMarginTop = '40px';
+        szv_section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    })
 
 })
