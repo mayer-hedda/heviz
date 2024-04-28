d0f45acb64ae254bf5841a9b9f2e835b64af6408
Mayer Hedda <mayer.hedda@gmail.com>
2024-04-28 14:12:34 +0200

    api update (unpublishBook)

--- Api/index.js
+++ Api/index.js
@@ -1587,6 +1587,64 @@ async function publishBook(raw) {
 }
 
 
+/**
+ * @param {JSON} raw = {
+ *      "id": 1             (bookId)
+ *  }
+ * 
+ * @return
+    * 200: Successfully unpublish this book
+    * 
+    * 401:
+        * User hasn't token
+        * Invalid token
+        * The token has expired
+    * 
+    * 403: User is not a publisher user
+    * 
+    * 422: error
+ */
+async function publishBook(raw) {
+    var myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/json");
+
+    var storedToken = localStorage.getItem("Token");
+    if (storedToken) {
+        myHeaders.append("Token", storedToken);
+    }
+
+    var postData = JSON.stringify(raw);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: postData,
+        redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch("http://127.0.0.1:8080/CyberRead-1.0-SNAPSHOT/webresources/book/unpublishBook", requestOptions);
+
+        if (response.status == 422) {
+            return {
+                status: response.status,
+                data: await response.json()
+            }
+        } else if (response.status == 401 || response.status == 403) {
+            return {
+                status: response.status,
+                data: await response.text()
+            }
+        }
+
+        return { status: response.status }
+    } catch (error) {
+        return { error: error }
+    }
+}
+
+
 
 // ----- PROFILES -----
 
